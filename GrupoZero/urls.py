96b89f875c367ac3b2b5c0d59e560f429d1a875a
mariajosecq <cote21cq@gmail.com>
2024-06-01 03:19:55 -0400

    Se modifica ruta de css, bootstrap, img y js en todos los html. Se elimina index.html. Se corrige paths de urls.py en la parte de views.

--- GrupoZero/urls.py
+++ GrupoZero/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('principal', views.principal, name='principal'),
     path('perfil_usuario', views.perfil_usuario, name='perfil_usuario'),
-    path('perfil_admin', views.index, name='perfil_admin'),
-    path('detalle_obra', views.index, name='detalle_obra'),
-    path('detalle_autor', views.index, name='detalle_autor'),
-    path('detalle_autor2', views.index, name='detalle_autor2'),
-    path('detalle_autor3', views.index, name='detalle_autor3'),
-    path('blog', views.index, name='blog'),
+    path('perfil_admin', views.perfil_admin, name='perfil_admin'),
+    path('detalle_obra', views.detalle_obra, name='detalle_obra'),
+    path('detalle_autor', views.detalle_autor, name='detalle_autor'),
+    path('detalle_autor2', views.detalle_autor2, name='detalle_autor2'),
+    path('detalle_autor3', views.detalle_autor3, name='detalle_autor3'),
+    path('blog', views.blog, name='blog'),
 
 ]
